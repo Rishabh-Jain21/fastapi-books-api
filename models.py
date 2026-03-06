87804dc59be274249a085320769e310ed80c3797
Rishabh Jain <rishabh9843@gmail.com>
2026-03-06 13:54:58 +0530

    Updated models to use mapped_column for type hinting

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
 from database import Base
 
-class Book(Base):
-    __tablename__="books"
 
-    id=Column(Integer,primary_key=True,index=True)
-    title=Column(String,index=True)
-    author=Column(String)
-    year=Column(Integer)
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String, index=True)
+    author: Mapped[str] = mapped_column(String)
+    year: Mapped[int] = mapped_column(Integer)
